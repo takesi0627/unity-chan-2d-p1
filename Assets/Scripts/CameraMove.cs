b2a9c81195f08ff59d156d4f35e8d0c245eb70ad
Nien Wu Juan <woody70825@gmail.com>
2018-11-21 10:48:30 +0900

    Camaera Scrolling

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+    [SerializeField] float moveSpeed = 0.6f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0, moveSpeed, 0));
+	}
+}
--- Assets/UnityChan/2DUnitychan/Scenes/Scripts/BattleSpriteAction.cs
+++ Assets/UnityChan/2DUnitychan/Scenes/Scripts/BattleSpriteAction.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityStandardAssets.CrossPlatformInput;
 
 public class BattleSpriteAction : MonoBehaviour
 {
